49a0b48bc7a3887d6a4990b428374e2d3640b418
calvinburkholder <147562997+calvinburkholder@users.noreply.github.com>
2026-03-17 07:11:25 +0000

    Func in tests

--- HW6_tests.py
+++ HW6_tests.py
@@ -1,4 +1,19 @@
-from HW6 import turnips
+def turnips(q):
+    n = len(q)
+    if n == 0:
+        return 0
+    memo = [0]*(n+1)
+
+    memo[0] = 0
+    if n >= 1:
+        memo[1] = q[0]
+    if n >= 2:
+        memo[2] = q[1]
+
+    for i in range(3, n+1):
+        memo[i] = q[i-1] + max(memo[:i-2])
+
+    return max(memo)
 
 def test_best_quality_1():
     assert turnips([5,1,2,8]) == 13
